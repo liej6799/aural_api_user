fd9626ec6b43db1fcbc66a51087ca44799c3f74e
Joes Lie <liej6799@protonmail.com>
2019-08-17 22:44:48 +0800

    password checking and structure has been made

--- Controllers/RegisterController.cs
+++ Controllers/RegisterController.cs
@@ -4,24 +4,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using aural_api_user.Models;
 using Microsoft.AspNetCore.Mvc;
-
+using static aural_model_lib.User.UserApiModel;
 namespace aural_api_user.Controllers
 {
+    /// <summary>
+    /// Controller to handle all the registration process made from user. 
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class RegisterController : ControllerBase
     {
         // POST Register User
         [HttpPost]
-        public void Post([FromBody] UserModel userModel)
+        public ActionResult<RegisterOutputModel> Post([FromBody] UserApiRegisterModel userApiRegisterModel)
         {
-            var a = userModel; 
+            var res = userApiRegisterModel.ValidationResult();
+            if (res == "")
+            {
+                // No Error
+                return Content("No Error");
+            }
+            else
+            {
+                return new RegisterOutputModel
+                {
+                    StatusCode = 1,
+                    StatusMessage = "Invalid Format Provided",
+                    ErrorMessages = res
+                };
+            }
         }
 
         [HttpGet]
-        public ActionResult<string> Get()
+        public ActionResult Get()
         {
-            return "You have entered the register section.";
+            return Content("You have entered the registration page");
         }
     }
 }
---
+++ Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aural_api_user.Models
+{
+    public class BaseModel
+    {
+        public int StatusCode { get; set; }
+        public string StatusMessage { get; set; }
+    }
+}
